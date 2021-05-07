776c73bedd6ec3763fce6b61101c28d51c8067a3
rlatjdus0814 <rlatjdus0814@naver.com>
2021-05-07 13:49:25 +0900

    중간고사 code review

--- midTest/DataTest.js
+++ midTest/DataTest.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-const DataTest = ({id, name, foo}) => { //props로 사용할 수 있도록 변수 지정
+const DataTest = (props) => { //props로 사용할 수 있도록 변수 지정
+  let {id} = props
+  let {name} = props
+  let {foo} = props //Midterm에서 전달받은 props를 구조분해 할당
+
   foo = foo.map((item, i) => {
     return (
-      <Text>{item}</Text>
+      <Text key={i}>{item}</Text> //배열 key설정 / 렌더링할 때 key을 이용해 어떤 원소가 변경되었는지 빠르게 감지하기 위해 사용됨
     )
   }) //배열 foo에 있는 데이터를 <Text>태그에 넣어 리턴
 
   return ( //Midterm 컴포넌트로 리턴
     <View>
-      {foo}
+      <Text>
+        학번: {id}
+        {'\n'}
+        이름: {name}
+      </Text>
+      {foo} 
     </View>
   )
 }
--- midTest/Midterm.js
+++ midTest/Midterm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import DataTest from './DataTest'
+import { View } from 'react-native'
+import DataTest from './DataTest' //DataTest컴포넌트 import해주기
 
 class Midterm extends Component {
   constructor() {
@@ -8,17 +8,15 @@ class Midterm extends Component {
     this.state = {
       id: '201930301',
       name: '김서연',
-      foo: [1,2,3,4,5],
     }
   }
 
   render(){
-    const { id, name, foo } = this.state
+    const { id, name } = this.state //구조분해 할당
     return(
       <View>
-        <Text>학번 : {id}</Text>
-        <Text>이름 : {name}</Text>
-        <DataTest id={id} name={name} foo={foo}></DataTest>
+        {/* DataTest컴포넌트로 전달 */}
+        <DataTest id={id} name={name} foo={[1,2,3,4,5]}></DataTest> 
       </View>
     )
   }

d01d2e42cb68437c4f013abb7df86183d40f5dd0
rlatjdus0814 <kim01093314735@gmail.com>
2021-05-14 14:34:49 +0900

    4.10 - 프로필 카드 border속성

--- app/App.js
+++ app/App.js
@@ -1,66 +1,48 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Example style={{borderRadius: 20}}>
-          <CenteredText>
-            Example 1:{"\n"} 네 모서리가 둥근 사각형
-          </CenteredText> 
-        </Example>
-        <Example style={{borderTopRightRadius: 60, borderBottomRightRadius: 60}}>
-          <CenteredText>
-            Example 2:{"\n"} 오른쪽 두 모서리가 둥근 사각형
-          </CenteredText> 
-        </Example>
-        <Example style={{borderTopLeftRadius: 30, borderBottomRightRadius: 30}}>
-          <CenteredText>
-            Example 3:{"\n"} 대각선 반대편 모서리가 둥근 사각형
-          </CenteredText> 
-        </Example>
-        <Example style={{borderRadius: 60}}>
-          <CenteredText>
-            Example 4:{"\n"} 원
-          </CenteredText> 
-        </Example>
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            <Image style={styles.cardImage} source={require('./user.png')} />
+          </View>
+        </View>
       </View>
     );
   }
 }
 
-const Example = (props) => (
-  <View style={[styles.example, props.style]}>
-    {props.children}
-  </View>
-);
-
-const CenteredText = (props) => (
-  <Text style={[styles.centeredText, props.style]}>
-    {props.children}
-  </Text>
-);
+const profileCardColor = 'dodgerblue';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  example: {
+  cardContainer: {
+    width: 300,
+    height: 400,
+    backgroundColor: profileCardColor,
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+  },
+  cardImageContainer: {
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: 'black',
     width: 120,
     height: 120,
-    marginLeft: 20,
-    marginBottom: 20,
-    backgroundColor: 'grey',
-    borderWidth: 2,
-    justifyContent: 'center',
+    borderRadius: 60,
   },
-  centeredText: {
-    textAlign: 'center',
-    margin: 10,
+  cardImage: {
+    width: 80,
+    height: 80,
   }
 });
 
